8e66a5f39d0ed67df385b81f5542a9171514b8a6
rusnel <rleikute@gmail.com>
2026-03-04 22:37:18 +0200

    doing tests

--- Assets/Scripts/Testas.cs
+++ Assets/Scripts/Testas.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Testas : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int testas = 0;
     void Start()
     {
         
