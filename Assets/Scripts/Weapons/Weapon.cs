6e68869d0ecd99eb353d33f9bb5f7553c5ec1b1d
Arnealis <Arnealis2005@gmail.com>
2026-05-27 23:02:20 +0300

    More changes + builds

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Unity.IO.LowLevel.Unsafe;
-using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
